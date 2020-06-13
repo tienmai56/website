c2e780d38b39ec133ff74db77a347d1ddb3ecedb
Vo Van Nghia <vovannghia2409@gmail.com>
2020-06-13 16:06:18 +0700

    Updates

---
+++ precache-manifest.c3abebd98debbedc94032c8b59b7dcfb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8002e523d7d8b4162971d12d8586a3e",
+    "url": "/shecode-website-react/index.html"
+  },
+  {
+    "revision": "082999b9db6749c02635",
+    "url": "/shecode-website-react/static/css/main.89a4adef.chunk.css"
+  },
+  {
+    "revision": "9e60ca7024e7af6938e5",
+    "url": "/shecode-website-react/static/js/2.a97462e3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/shecode-website-react/static/js/2.a97462e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "082999b9db6749c02635",
+    "url": "/shecode-website-react/static/js/main.9f2ccaa3.chunk.js"
+  },
+  {
+    "revision": "a395ee65f6a12fcc203e",
+    "url": "/shecode-website-react/static/js/runtime-main.8e5b0923.js"
+  }
+]);
