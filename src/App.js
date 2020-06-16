e62197a607530f827a63ab86b712e96c29a32e52
Vo Van Nghia <vovannghia2409@gmail.com>
2020-06-16 19:00:36 +0700

    fix for cloud-front

--- src/App.js
+++ src/App.js
@@ -1,38 +1,18 @@
 import React from 'react';
 import './App.css';
 import './style/icon-font.css';
-
 import { Route, Switch } from "react-router-dom";
+
+import AppSwitcher from './AppSwitcher';
 
 import NavBar from './Components/NavBar';
 import Footer from './Components/Footer';
-
-import Blog from './Components/Blog/Blog';
-import HomePage from './Components/HomePage/HomePage';
-import Event from './Components/Event/Event';
-import Hackathon from './Components/Hackathon/Hackathon';
-import TechMarathon from './Components/TechMarathon/TechMarathon';
-
-import Staffs from './Components/Staffs/Staffs';
-import Contact from './Components/Contact/Contact';
-import FAQ from './Components/FAQ/FAQ';
-import Sponsors from './Components/Sponsors/Sponsors';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <Switch>
-        <Route exact={true} path="/" component={HomePage}/>
-        <Route path="/blog" component={Blog}/>
-        <Route path="/event" component={Event} />
-        <Route path="/hackathon" component={Hackathon} />
-        <Route path="/tech-marathon" component={TechMarathon} />
-        <Route path="/staffs" component={Staffs} />
-        <Route path="/contact" component={Contact} />
-        <Route path="/faqs" component={FAQ} />
-        <Route path="/sponsors" component={Sponsors} />
-      </Switch>
+      <Route exact path="(/|/blog|/event|/hackathon|/tech-marathon|/staffs|/contact|/faqs|/sponsors)" component={AppSwitcher}/>
       <Footer />
     </div>
   );
---
+++ src/AppSwitcher.js
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from 'react';
+
+import HomePage from './Components/HomePage/HomePage';
+import Blog from './Components/Blog/Blog';
+import Event from './Components/Event/Event';
+import Hackathon from './Components/Hackathon/Hackathon';
+import TechMarathon from './Components/TechMarathon/TechMarathon';
+import Staffs from './Components/Staffs/Staffs';
+import Contact from './Components/Contact/Contact';
+import FAQ from './Components/FAQ/FAQ';
+import Sponsors from './Components/Sponsors/Sponsors';
+
+const components = ["/", "/blog", "/event", "/hackathon", "/tech-marathon", "/staffs", "/contact", "/faqs", "/sponsors"];
+
+const AppSwitcher = (props) => {
+  const [displayComponent, setDisplayComponents] = useState(components[0]);
+  useEffect(() => {
+    setDisplayComponents(props.location.pathname);
+  }, [props.location.pathname]);
+  return(
+    <div className="App">
+      <div hidden={displayComponent !== components[0]}>
+        <HomePage/>
+      </div>
+      <div hidden={displayComponent !== components[1]}>
+        <Blog/>
+      </div>
+      <div hidden={displayComponent !== components[2]}> 
+        <Event />
+      </div>
+      <div hidden={displayComponent !== components[3]}>
+        <Hackathon />
+      </div>
+      <div hidden={displayComponent !== components[4]}>
+        <TechMarathon />
+      </div>
+      <div hidden={displayComponent !== components[5]}>
+        <Staffs />
+      </div>
+      <div hidden={displayComponent !== components[6]}>
+        <Contact />
+      </div>
+      <div hidden={displayComponent !== components[7]}>
+        <FAQ />
+      </div>
+      <div hidden={displayComponent !== components[8]}>
+        <Sponsors />
+      </div>
+    </div>
+  );
+};
+
+export default AppSwitcher;
