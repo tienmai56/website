eb784268b36d8e23731d7638c12c5f1fe583145a
Vo Van Nghia <vovannghia2409@gmail.com>
2020-06-17 11:56:38 +0700

    Fix staff

--- src/Components/Staffs/Staffs.js
+++ src/Components/Staffs/Staffs.js
@@ -19,9 +19,9 @@ const StaffsRow = styled.div`
     border-top: 1px solid #3f3f3f; 
   }
   &:not(:last-child) {
-		margin-bottom: 8rem;
+		margin-bottom: 4rem;
 		@media only screen and (max-width: 56.25em) {
-			margin-bottom: 6rem;
+			margin-bottom: 2rem;
 		}
   }
   @media only screen and (max-width: 56.25em) {
@@ -82,7 +82,10 @@ const StaffsRow = styled.div`
 `;
 
 const StaffsRoleRow = styled(StaffsRow)`
-  margin-top: 15rem;
+  margin-top: 10rem;
+  @media only screen and (max-width: 56.25em) {
+    margin-top: 5rem;
+  }
 `;
 
 const StaffsHeader = styled.h2`
@@ -177,110 +180,133 @@ const StaffsRole= styled(StaffsName)`
 `;
 
 const staffsMap = {
-  "Director": [
+  "Founder": [
     {
       name: "Mai Phương Thủy Tiên",
       image: "HN/Director_Mai_Phuong_Thuy_Tien.jpg",
-    }
+      content: "Founder"
+    },
+    {
+      name: "Nguyễn Huyền My",
+      image: "",
+      content: "Founder"
+    },
   ],
   "Program Manager": [
     {
       name: "Trần Tuệ Nhi",
       image: "HN/PM_Tran_Tue_Nhi.jpg",
+      content: "Head of Hackathon - Hà Nội",
     },
     {
       name: "Nguyễn Vũ Ngân Hà",
       image: "HN/PM_Nguyen_Vu_Ngan_Ha.jpg",
-    },
-    {
-      name: "Nguyễn Huyền My",
-      image: "HN/PM_Nguyen_Vu_Ngan_Ha.jpg",
+      content: "Head of Tech Marathon - Hà Nội",
     },
     {
       name: "Nguyễn Thế Anh",
-      image: "HN/PM_Nguyen_The_Anh.png"
+      image: "HN/PM_Nguyen_The_Anh.png",
+      content: "Head of Tech Talk - Hà Nội",
     },
     {
       name: "Bùi Võ Ngọc Quế Trân",
       image: "HCM/PM_Bui_Vo_Ngoc_Que_Tran.jpg",
+      content: "Head of Hackathon - Hồ Chí Minh"
     },
     {
       name: "Trần Khánh Vy",
-      image: "HCM/PM_Tran_Khanh_Vy.jpg"
+      image: "HCM/PM_Tran_Khanh_Vy.jpg",
+      content: "Head of IT Fair - Hồ Chí Minh"
     },
     {
       name: "Hoàng Diệu Khánh",
-      image: "HCM/PM_Hoang_Dieu_Khanh.jpg"
+      image: "HCM/PM_Hoang_Dieu_Khanh.jpg",
+      content: "Head of Workshop - Hồ Chí Minh",
     }
   ],
   "Technology": [
     {
       name: "Nguyễn Hương Giang",
       image: "HN/Tech_Nguyen_Huong_Giang.jpg",
+      content: "Hà Nội"
     },
     {
       name: "Nhâm Bảo Khanh",
       image: "HN/Tech_Nham_Bao_Khanh.png",
+      content: "Hà Nội"
     },
     {
       name: "Vũ Quang Huy",
-      image: "HN/Tech_Nguyen_Huong_Giang.jpg",
+      image: "",
+      content: "Hà Nội"
     },
     {
       name: "Võ Văn Nghĩa",
       image: "HN/Tech_Vo_Van_Nghia.jpg",
+      content: "Hà Nội"
     },
     {
       name: "Nguyễn Trần Phước Thịnh",
       image: "HCM/Tech_Nguyen_Tran_Phuoc_Thinh.jpg",
+      content: "Hồ Chí Minh"
     },
     {
       name: "Nguyễn Vũ Anh Thư",
       image: "HCM/Tech_Nguyen_Vu_Anh_Thu.jpg",
+      content: "Hồ Chí Minh"
     }
   ],
   "Human Resources": [
     {
       name: "Nguyễn Doãn Hoàng",
       image: "HN/HR_Nguyen_Doan_Hoang.png",
+      content: "Hà Nội",
     },
     {
       name: "Nguyễn Thị Đinh Thủy",
       image: "HCM/HR_Nguyen_Thi_Dinh_Thuy.jpg",
+      content: "Hồ Chí Minh"
     },
     {
       name: "Văn Thị Kim Ân",
       image: "HCM/HR_Van_Thi_Kim_An.jpg",
+      content: "Hồ Chí Minh"
     }
   ],
   "Marketing": [
     {
       name: "Nguyễn Ngọc Mai",
       image: "HN/Marketing_Nguyen_Ngoc_Mai.jpg",
+      content: "Hà Nội",
     },
     {
       name: "Huỳnh Nguyễn Ngân Thy",
-      image: "HCM/Marketing_Huynh_Nguyen_Ngan_Thy.jpg"
+      image: "HCM/Marketing_Huynh_Nguyen_Ngan_Thy.jpg",
+      content: "Hồ Chí Minh"
     }
   ],
   "Partnership": [
     {
       name: "Nguyễn Thạc Linh Chi",
-      image: "HN/Partnership_Nguyen_Thac_Linh_Chi.png"
+      image: "HN/Partnership_Nguyen_Thac_Linh_Chi.png",
+      content: "Hà Nội",
     },
     {
       name: "Nguyễn Tôn Vành Khuyên",
-      image: "HCM/Partnership_Nguyen_Ton_Vanh_Khuyen .jpg"
+      image: "HCM/Partnership_Nguyen_Ton_Vanh_Khuyen .jpg",
+      content: "Hồ Chí Minh"
     }
   ],
   "Designer": [
     {
       name: "Bùi Minh Hiếu",
-      image: "HN/Design_Bui_Minh_Hieu.jpg"
+      image: "HN/Design_Bui_Minh_Hieu.jpg",
+      content: "Hà Nội",
     },
     {
       name: "Nguyễn Ngọc Thảo Nhi",
-      image: "HCM/Design_Nguyen_Ngoc_Thao_Nhi.png"
+      image: "HCM/Design_Nguyen_Ngoc_Thao_Nhi.png",
+      content: "Hà Nội",
     }
   ]
 }
@@ -301,7 +327,7 @@ const generateStaffsComponent = (staffsMap) => {
                 <StaffsImage alt={staff.name} src={"https://raw.githubusercontent.com/shecodesvietnam/asserts/master/2020/staffs/" + staff.image}/>
               </StaffsImageBox>
               <StaffsName>{staff.name}</StaffsName>
-              <StaffsRole>- {role}</StaffsRole>
+              <StaffsRole>- {staff.content}</StaffsRole>
             </StaffsColumn>
           )
         }
