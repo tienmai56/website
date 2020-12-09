39789e903c8e826d0a1397b5fad139b44f2c075f
Vu Huy <vuhuycto@gmail.com>
2020-12-10 00:27:14 +0700

    remove basename route

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import ScrollToTop from "./ScrollToTop";
 
 ReactDOM.render(
   <React.StrictMode>
-    <HashRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter>
       <ScrollToTop />
       <App />
     </HashRouter>
